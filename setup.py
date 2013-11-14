26aff9959520891881e0f029e0813c82f9d0b076
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2013-11-14 16:29:27 +0100

    Packaging nagios probes

--- setup.py
+++ setup.py
@@ -1,57 +1,64 @@
 #!/usr/bin/env python
 
+import sys, os, os.path, shlex, subprocess
+from subprocess import Popen as execScript
 from distutils.core import setup
-import ConfigParser
+from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
-pkg_version = '0.0.0'
-try:
-    parser = ConfigParser.ConfigParser()
-    parser.read('setup.cfg')
-    pkg_version = parser.get('global','pkgversion')
-except:
-    pass
+pkg_name = 'emi-cream-nagios'
+pkg_version = '1.1.1'
+pkg_release = '2'
 
-config_list = [
-               "config/emi.nagios-jobmngt.conf"
-              ]
+source_items = "setup.py src"
+
+class bdist_rpm(_bdist_rpm):
+
+    def run(self):
+
+        topdir = os.path.join(os.getcwd(), self.bdist_base, 'rpmbuild')
+        builddir = os.path.join(topdir, 'BUILD')
+        srcdir = os.path.join(topdir, 'SOURCES')
+        specdir = os.path.join(topdir, 'SPECS')
+        rpmdir = os.path.join(topdir, 'RPMS')
+        srpmdir = os.path.join(topdir, 'SRPMS')
+        
+        cmdline = "mkdir -p %s %s %s %s %s" % (builddir, srcdir, specdir, rpmdir, srpmdir)
+        execScript(shlex.split(cmdline)).communicate()
+        
+        cmdline = "tar -zcf %s %s" % (os.path.join(srcdir, pkg_name + '.tar.gz'), source_items)
+        execScript(shlex.split(cmdline)).communicate()
+        
+        specOut = open(os.path.join(specdir, pkg_name + '.spec'),'w')
+        cmdline = "sed -e 's|@PKGVERSION@|%s|g' -e 's|@PKGRELEASE@|%s|g' project/%s.spec.in" % (pkg_version, pkg_release, pkg_name)
+        execScript(shlex.split(cmdline), stdout=specOut, stderr=sys.stderr).communicate()
+        specOut.close()
+        
+        cmdline = "rpmbuild -ba --define '_topdir %s' %s.spec" % (topdir, os.path.join(specdir, pkg_name))
+        execScript(shlex.split(cmdline)).communicate()
+
 
 libexec_list = [
-                "src/CREAMCEDJS-probe",
-                "src/CREAMCE-probe"
-               ]
-
-package_list = [
-                "creammetrics",
-                "WNProbes/dirq",
-                "WNProbes/metric",
-                "WNProbes/mig",
-                "WNProbes/mig/messaging",
-                "WNProbes/mig/connection",
-                "WNProbes/mig/discovery",
-                "WNProbes/msgdirq",
-                "WNProbes/stomp"
+                "src/cream_allowedSubmission.py",
+                "src/cream_jobCancel.py", 
+                "src/cream_jobPurge.py", 
+                "src/cream_jobSubmit.py", 
+                "src/cream_serviceInfo.py"
                ]
 
 setup(
-      name='cream-nagios',
+      name=pkg_name,
       version=pkg_version,
       description='Nagios probe for the EMI CREAM and WN services',
-      long_description='''This package contains the following Nagios probes:
-CREAMCE-probe, CREAMCEDJS-probe, samtest-run.
-- The probes can run in active and/or passives modes (in Nagios sense).
-  Publication of passive test results from inside of probes can be done
-  via Nagios command file or NSCA.
-- On worker nodes Nagios is used as probes scheduler and executer. Metrics
-  results from WNs can be sent to Message Broker.''',
+      long_description='''This package contains a set of NAGIOS plugins  
+used to monitor a CREAM CE node.''',
       license='Apache Software License',
-      author='CREAM group',
       author_email='CREAM group <cream-support@lists.infn.it>',
-      packages=package_list,
-      package_dir = {'': 'src'},
+      py_modules=['cream'],
+      package_dir={'': 'src'},
       data_files=[
-                  ('etc/gridmon', config_list),
                   ('usr/libexec', libexec_list)
-                 ]
+                 ],
+      cmdclass={'bdist_rpm': bdist_rpm}
      )
 
 
